998ff5a4ae16252dafa719edc805a69989cc1fd3
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-01-30 01:04:08 +0100

    add rules if

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,25 +11,27 @@ const KEY = 'feedback-form-state';
 form.addEventListener('input', trottle(hendleTakeData, 500));
 form.addEventListener('submit', hendleSaveData);
 
-loadingPage();
-
 function hendleTakeData(event) {
-  loadingPage();
   data[event.target.name] = event.target.value;
   const dataValue = JSON.stringify(data);
   localStorage.setItem(KEY, dataValue);
+  loadingPage();
 }
 //   при перезавантаженні сторінки
 function loadingPage() {
-  if (data) {
-    input = JSON.parse(localStorage.getItem(KEY) || '');
-    message = JSON.parse(localStorage.getItem(KEY) || '');
+  if (localStorage.getItem(KEY)) {
+    input === JSON.parse(localStorage.getItem(KEY) || '');
+    message === JSON.parse(localStorage.getItem(KEY) || '');
   }
 }
 
 function hendleSaveData(event) {
   event.preventDefault();
-  event.currentTarget.reset();
-  console.log(localStorage.getItem(KEY));
-  localStorage.removeItem(KEY);
+  if (input.value === '' || message.value === '') {
+    alert(`Всі поля мають бути заповнені`);
+  } else {
+    event.currentTarget.reset();
+    console.log(localStorage.getItem(KEY));
+    localStorage.removeItem(KEY);
+  }
 }
