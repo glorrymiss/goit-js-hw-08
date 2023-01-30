c3f949d34b1f3416c603a463de3421d44533a11a
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-01-30 16:57:20 +0100

    add result

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,35 @@
-const trottle = require('lodash.throttle');
+// const trottle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
-const message = document.querySelector('textarea');
-console.log(form);
+const textarea = document.querySelector('textarea');
 
-// let data = {};
+let data = {};
+
 const KEY = 'feedback-form-state';
 
-form.addEventListener('input', trottle(hendleTakeData, 500));
+form.addEventListener('input', hendleTakeData);
 form.addEventListener('submit', hendleSaveData);
 
+//   при перезавантаженні сторінки
+input.value = JSON.parse(localStorage.getItem(KEY)).email || '';
+textarea.value = JSON.parse(localStorage.getItem(KEY)).message || '';
 function hendleTakeData(event) {
-  input.value = JSON.parse(localStorage.getItem(KEY)).email || '';
-  message.value = JSON.parse(localStorage.getItem(KEY)).message || '';
-  const data = {
-    email: form.elements.email.value,
-    message: form.elements.message.value,
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  data = {
+    email: email.value,
+    message: message.value,
   };
+
   const dataValue = JSON.stringify(data);
   localStorage.setItem(KEY, dataValue);
 }
-//   при перезавантаженні сторінки
 
 function hendleSaveData(event) {
   event.preventDefault();
-  if (input.value === '' || message.value === '') {
+  if (input.value === '' || textarea.value === '') {
     alert(`Всі поля мають бути заповнені`);
   } else {
     event.currentTarget.reset();
